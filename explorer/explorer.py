627b64ba0eefeb2a1cc1bad37424aff2b8ada5e3
isma <gayeismael22@gmail.com>
2026-05-28 16:13:42 +0200

    working explorer normally

--- explorer/explorer.py
+++ explorer/explorer.py
@@ -236,7 +236,7 @@ class BasicBlockExplorer(AbstractExplorer):
         self.statistics_technique.report_stats()
 
 
-class Nemesis():
+class NemesisOpenIPE():
     # Here the offset represents how much instructions we're supposed to let before running 
     def __init__(self, trace, offset=4):
         self.trace_file = trace
@@ -255,48 +255,101 @@ class Nemesis():
             return list(map(int, list(data)[0]))
         
 
+    # introduce a penalty if the destination is in the ROM
+    # We're supposed to be in IPE so when we write into memory it's always in the ROM
+    @staticmethod
+    def rom_penalty(instruction_parsed):
+        if isinstance(instruction_parsed, msp430_instrs.Instruction_CMP) or isinstance(instruction_parsed, msp430_instrs.Instruction_BIT):
+            return 0
+        return 1
+    
 
     @staticmethod
-    def get_instruction_length(instruction_parsed, instruction_length):
-        # TODO: see DADD (cycle++)
+    def get_src_real_mode(src_bits, mode_bits):
+        src = msp430_arch.ArchMSP430.register_index[int(src_bits, 2)]
+        src_mode = int(mode_bits, 2)
 
+        # Symbolic and Immediate modes use the PC as the source.
+        if src == 'pc':
+            if src_mode == msp430_arch.ArchMSP430.Mode.INDEXED_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE
+            elif src_mode == msp430_arch.ArchMSP430.Mode.INDIRECT_AUTOINCREMENT_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.IMMEDIATE_MODE
+        # Resolve the constant generator stuff.
+        elif src == 'cg':
+            if src_mode == msp430_arch.ArchMSP430.Mode.REGISTER_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE0
+            elif src_mode == msp430_arch.ArchMSP430.Mode.INDEXED_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE1
+            elif src_mode == msp430_arch.ArchMSP430.Mode.INDIRECT_REGISTER_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE2
+            else:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE_NEG1
+        # If you use the SR as the source. things get weird.
+        elif src == 'sr':
+            if src_mode == msp430_arch.ArchMSP430.Mode.INDEXED_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE
+            elif src_mode == msp430_arch.ArchMSP430.Mode.INDIRECT_REGISTER_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE4
+            elif src_mode == msp430_arch.ArchMSP430.Mode.INDIRECT_AUTOINCREMENT_MODE:
+                src_mode = msp430_arch.ArchMSP430.Mode.CONSTANT_MODE8
+
+        return src_mode
+    
+
+    @staticmethod
+    def get_dst_real_mode(dst_bits, mode_bits):
+        dst = msp430_arch.ArchMSP430.register_index[int(dst_bits, 2)]
+        dst_mode = int(mode_bits, 2)
+
+        # Using sr as the dst enables "absolute addressing"
+        if dst == 'sr' and dst_mode == msp430_arch.ArchMSP430.Mode.INDEXED_MODE:
+            dst_mode = msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE
+
+        return dst_mode
+
+    def get_instruction_length(self, instruction_parsed, instruction_length):
+        # TODO: see DADD (cycle++)
         if isinstance(instruction_parsed, msp430_instrs.Type1Instruction):
             logger.debug("Instruction is of format 2")
 
             if isinstance(instruction_parsed, msp430_instrs.Instruction_RETI):
                 return 5
 
-            As = int(instruction_parsed.data['A'])    
+            As = self.get_src_real_mode(instruction_parsed.data['s'], instruction_parsed.data['A'])    
             match As:
                 case msp430_arch.ArchMSP430.Mode.REGISTER_MODE | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE0 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE1 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE2 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE4 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE8 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE_NEG1:
                     if isinstance(instruction_parsed, msp430_instrs.Instruction_PUSH):
-                        return 3
+                        return 3 + self.rom_penalty(instruction_parsed)
                     elif isinstance(instruction_parsed, msp430_instrs.Instruction_CALL):
-                        return 4
+                        # 4 on MSP430
+                        return 3 + self.rom_penalty(instruction_parsed)
                     else:
                         return 1
 
                 case msp430_arch.ArchMSP430.Mode.INDEXED_MODE | msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE | msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
                     if isinstance(instruction_parsed, msp430_instrs.Instruction_PUSH):
-                        return 5
+                        return 5 + self.rom_penalty(instruction_parsed)
                     elif isinstance(instruction_parsed, msp430_instrs.Instruction_CALL):
-                        return 5
+                        return 5 + self.rom_penalty(instruction_parsed)
                     else:
-                        return 4
+                        return 4 + self.rom_penalty(instruction_parsed)
                 case msp430_arch.ArchMSP430.Mode.INDIRECT_REGISTER_MODE:
                     if isinstance(instruction_parsed, msp430_instrs.Instruction_PUSH):
-                        return 4
+                        return 4 + self.rom_penalty(instruction_parsed)
                     elif isinstance(instruction_parsed, msp430_instrs.Instruction_CALL):
-                        return 4
+                        return 4 + self.rom_penalty(instruction_parsed)
                     else:
-                        return 3
+                        return 3 + self.rom_penalty(instruction_parsed)
                 case msp430_arch.ArchMSP430.Mode.INDIRECT_AUTOINCREMENT_MODE |  msp430_arch.ArchMSP430.Mode.IMMEDIATE_MODE:
                     if isinstance(instruction_parsed, msp430_instrs.Instruction_PUSH):
-                        return 4
+                        # 5 on MSP430
+                        return 4 + self.rom_penalty(instruction_parsed)
                     elif isinstance(instruction_parsed, msp430_instrs.Instruction_CALL):
-                        return 5
+                        # 5 on MSP430
+                        return 4 + self.rom_penalty(instruction_parsed)
                     else:
-                        return 3
+                        return 3 + self.rom_penalty(instruction_parsed)
             return 0
         
         elif isinstance(instruction_parsed, msp430_instrs.Type2Instruction):
@@ -305,43 +358,44 @@ class Nemesis():
         
         elif isinstance(instruction_parsed, msp430_instrs.Type3Instruction):
             logger.debug("Instruction is of format 1")
-
-            As = int(instruction_parsed.data['A'])
-            Ad = int(instruction_parsed.data['a'])
-            print(instruction_length, As, Ad, instruction_parsed.data)
+            
+            As = self.get_src_real_mode(instruction_parsed.data['s'], instruction_parsed.data['A'])
+            Ad = self.get_src_real_mode(instruction_parsed.data['d'], instruction_parsed.data['a'])
             d = int(instruction_parsed.data['d'], 2)
+
+            dadd_penalty = 1 if isinstance(instruction_parsed, msp430_instrs.Instruction_DADD) else 0
             match As:
                 case msp430_arch.ArchMSP430.Mode.REGISTER_MODE | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE0 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE1 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE2 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE4 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE8 | msp430_arch.ArchMSP430.Mode.CONSTANT_MODE_NEG1:
                     if Ad == msp430_arch.ArchMSP430.Mode.REGISTER_MODE:
-                        if d == msp430_arch.ArchMSP430.register_index[d] == 'pc':
-                            return 2
-                        else:
-                            return 1
+                        if msp430_arch.ArchMSP430.register_index[d] == 'pc':
+                            return 2 + dadd_penalty
+                        return 1 + dadd_penalty
                     elif Ad == msp430_arch.ArchMSP430.Mode.INDEXED_MODE or Ad == msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE or Ad == msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
-                        return 4
+                        return 4 + self.rom_penalty(instruction_parsed) + dadd_penalty
                 case msp430_arch.ArchMSP430.Mode.INDEXED_MODE | msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE | msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
                     if Ad == msp430_arch.ArchMSP430.Mode.REGISTER_MODE:
-                        return 3
+                        if msp430_arch.ArchMSP430.register_index[d] == 'pc':
+                            return 4 + dadd_penalty
+                        return 3 + dadd_penalty
                     elif Ad == msp430_arch.ArchMSP430.Mode.INDEXED_MODE or Ad == msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE or Ad == msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
-                        return 6
+                        return 6 + self.rom_penalty(instruction_parsed) + dadd_penalty
                 case msp430_arch.ArchMSP430.Mode.INDIRECT_REGISTER_MODE:
                     if Ad == msp430_arch.ArchMSP430.Mode.REGISTER_MODE:
-                        return 2
+                        if msp430_arch.ArchMSP430.register_index[d] == 'pc':
+                            return 3 + dadd_penalty
+                        return 2 + dadd_penalty
                     elif Ad == msp430_arch.ArchMSP430.Mode.INDEXED_MODE or Ad == msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE or Ad == msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
-                        return 5
+                        return 5 + self.rom_penalty(instruction_parsed) + dadd_penalty
                 case msp430_arch.ArchMSP430.Mode.INDIRECT_AUTOINCREMENT_MODE |  msp430_arch.ArchMSP430.Mode.IMMEDIATE_MODE:
                     if Ad == msp430_arch.ArchMSP430.Mode.REGISTER_MODE:
                         if instruction_length == 2:
-                            return 2
-                        elif d == msp430_arch.ArchMSP430.register_index[d] == 'pc':
-                            return 3
-                        else:
-                            return 1
+                            return 2 + dadd_penalty
+                        elif msp430_arch.ArchMSP430.register_index[d] == 'pc':
+                            return 3 + dadd_penalty
+                        return 1 + dadd_penalty
                     elif Ad == msp430_arch.ArchMSP430.Mode.INDEXED_MODE or Ad == msp430_arch.ArchMSP430.Mode.SYMBOLIC_MODE or Ad == msp430_arch.ArchMSP430.Mode.ABSOLUTE_MODE:
-                        return 5
+                        return 5 + self.rom_penalty(instruction_parsed) + dadd_penalty
 
-                    
-            return 0
         return -1
     
 
@@ -367,7 +421,10 @@ class Nemesis():
             lifter.pp_disas()
 
             if 0 <= index_in_trace < len(self.cftrace):
-                nb_true = self.get_instruction_length(lifter.decode()[0], instr_size // 2)
+                instr_parsed = lifter.decode()[0]
+                instr_parsed.disassemble()
+
+                nb_true = self.get_instruction_length(instr_parsed, instr_size // 2)
 
                 logger.info(f"Number of cycles for this instruction: {nb_true}")
                 logger.info(f"Supposed number of cycles according to trace {self.cftrace[index_in_trace]}")
@@ -398,7 +455,7 @@ class Nemesis():
 
 class BasicBlockScaseExplorer(AbstractExplorer):
     def __init__(self, trace, binary_path='', action=UserAction.NONE, base_addr=0, angr_backend='elf', angr_arch='x86_64'):
-        self.nemesis_pruner = Nemesis(trace)
+        self.nemesis_pruner = NemesisOpenIPE(trace)
         return super(BasicBlockScaseExplorer, self).__init__(binary_path, action, base_addr, angr_backend, angr_arch)
 
 
