7e4962ffa172b11e1369811e41094526fe922cef
isma <gayeismael22@gmail.com>
2026-06-08 11:41:09 +0200

    update requirements

--- explorer/memory/EnclaveAwareMemory.py
+++ explorer/memory/EnclaveAwareMemory.py
@@ -1,7 +1,7 @@
 from angr.storage.memory_mixins import PagedMemoryMixin, SymbolicMergerMixin, DefaultFillerMixin, UltraPagesMixin, \
     PrivilegedPagingMixin, DictBackerMixin, ClemoryBackerMixin, ConcreteBackerMixin, StackAllocationMixin, \
     DirtyAddrsMixin, ConvenientMappingsMixin, ConditionalMixin, ActionsMixinLow, AddressConcretizationMixin, \
-    SizeNormalizationMixin, SizeConcretizationMixin, UnderconstrainedMixin, ActionsMixinHigh, InspectMixinHigh, \
+    SizeNormalizationMixin, SizeConcretizationMixin, UnderconstrainedMixin, ActionsMixinHigh, InspectMixin, \
     DataNormalizationMixin, NameResolutionMixin, UnwrapperMixin, SmartFindMixin
 
 from explorer.memory.EnclaveAwareMixin import EnclaveAwareMixin
@@ -15,7 +15,7 @@ class EnclaveAwareMemory(
     NameResolutionMixin, # description: allows you to provide register names as load addresses, and will automatically translate this to an offset and size.
     DataNormalizationMixin, # description: Normalizes the data field for a store and the fallback field for a load to be BVs.
     # SimplificationMixin, # hooks stores and first calls state.solver.simplify(data) if options.SIMPLIFY_[MEMORY/REGISTER]_WRITES is set
-    InspectMixinHigh, # The logic to inspect memory/register reads/writes --> calls ._inspect before/after.
+    InspectMixin, # The logic to inspect memory/register reads/writes --> calls ._inspect before/after.
     ActionsMixinHigh,
     UnderconstrainedMixin,
     SizeConcretizationMixin,
